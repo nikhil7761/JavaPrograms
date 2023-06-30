35ed3fbf79e2d64d4aed09dbb94024ec7554b0db
Mr.Nikhil <115720074+nikhil7761@users.noreply.github.com>
2023-06-30 16:09:42 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,23 @@
+package commm.java;
+
+public class Alphabets {
+	
+	
+	public static void main(String[]args)
+	{
+		
+		char ch;
+		
+		for(ch='A';ch<'z';ch++) {
+			
+			System.out.println(ch);
+		}
+		
+		
+		
+		
+	}
+	
+	
+
+}
