1bb51ce4b7c8c115397b998b3ec9bb6166086fbf
Mr.Nikhil <115720074+nikhil7761@users.noreply.github.com>
2023-07-01 15:36:20 +0530

    Add files via upload

---
+++ InvertString.java
@@ -0,0 +1,18 @@
+package com.problem.java;
+
+public class InvertString {
+
+	public static void main(String[]args)
+	{
+		
+	   StringBuffer b=new StringBuffer("Something");
+	   
+	   System.out.println(b.reverse());
+		
+		
+		
+	}
+	
+	
+	
+}
