be9e209fd5d2002e474932c81340bb618733096b
Mr.NIKHIL VARGUDE <115720074+nikhil7761@users.noreply.github.com>
2023-07-02 21:44:58 +0530

    Add files via upload

---
+++ IpAdress.java
@@ -0,0 +1,13 @@
+package com.problem.java;
+
+import java.net.InetAddress;
+
+public class IpAdress {
+	
+	public static void main(String[]args)throws Exception
+	{
+		
+		System.out.println(InetAddress.getLocalHost());
+	}
+
+}
