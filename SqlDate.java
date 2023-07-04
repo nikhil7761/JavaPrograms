29e2dec0c946620153eb3b86c86deff485de1b64
Mr.NIKHIL VARGUDE <115720074+nikhil7761@users.noreply.github.com>
2023-07-04 20:57:22 +0530

    Add files via upload

---
+++ SqlDate.java
@@ -0,0 +1,21 @@
+package com.problem.java;
+
+import java.sql.Date;
+
+public class SqlDate {
+
+	
+	public static void main(String[]args) {
+		
+		String str="2000-03-13";
+		
+		Date date=Date.valueOf(str);
+		
+		System.out.println(date);
+		
+		
+		
+		
+	}
+	
+}
